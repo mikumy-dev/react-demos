89b724181ba79b10e6d928d6e395dede28049395
zosoyi <cjsz321qq@gmail.com>
2022-03-08 21:17:24 +0900

    simplify the count function

--- 07-use-state/src/App.js
+++ 07-use-state/src/App.js
@@ -4,63 +4,48 @@ export default function App() {
     let [count, setCount] = React.useState(0)
     let [countHistoryRows, setCountHistoryRows] = React.useState([])
 
-    function addCount() {
+    function changeCount(operator) {
         setCount(prevCount => {
+            let newCount
+            switch (operator) {
+                case '+': 
+                    newCount = prevCount + 1
+                    break
+                case '-': 
+                    newCount = prevCount - 1
+                    break
+                case 'reset': 
+                    newCount = 0
+                    break
+                default:
+                    break
+            }
+
             setCountHistoryRows(prevArr => {
+                // add key to prevent the warning [Each child in a list should have a unique "key" prop.]
                 const row = (
-                    <tr>
+                    <tr key={new Date().getTime()}>
                         <td>{prevCount}</td>
-                        <td>+</td>
-                        <td>{prevCount + 1}</td>
+                        <td>{operator}</td>
+                        <td>{newCount}</td>
                     </tr>
                 )
                 return [...prevArr, row]
             })
-            return prevCount + 1
+            return newCount
         })
     }
 
-    function minusCount() {
-        setCount(prevCount => {
-            setCountHistoryRows(prevArr => {
-                const row = (
-                    <tr>
-                        <td>{prevCount}</td>
-                        <td>-</td>
-                        <td>{prevCount - 1}</td>
-                    </tr>
-                )
-                return [...prevArr, row]
-            })
-            return prevCount - 1
-        })
-    }
-
-    function resetCount() {
-        setCount(prevCount => {
-            setCountHistoryRows(prevArr => {
-                const row = (
-                    <tr>
-                        <td>{prevCount}</td>
-                        <td>reset</td>
-                        <td>0</td>
-                    </tr>
-                )
-                return [...prevArr, row]
-            })
-            return 0
-        })
-    }
-
+    // use arrow function in onClick except using the function itself changeCount('-') to prevent calling it at screen onload
     return (
         <>
             <div className='countBlock'>
                 <h1>Press + or - to change the number</h1>
                 <h2 className='count'>{count}</h2>
                 <div className='buttons'>
-                    <button onClick={minusCount}>-1</button>
-                    <button onClick={resetCount}>reset</button>
-                    <button onClick={addCount}>+1</button>
+                    <button onClick={() => changeCount('-')}>-1</button>
+                    <button onClick={() => changeCount('reset')}>reset</button>
+                    <button onClick={() => changeCount('+')}>+1</button>
                 </div>
             <div className='count-history'>
                 <h4>count history</h4>
