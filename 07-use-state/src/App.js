3c3a10d0373438d5b719474af2d4a471753bb889
zosoyi <cjsz321qq@gmail.com>
2022-03-08 20:56:18 +0900

    use state to update array

--- 07-use-state/src/App.js
+++ 07-use-state/src/App.js
@@ -2,28 +2,82 @@ import React from 'react'
 
 export default function App() {
     let [count, setCount] = React.useState(0)
+    let [countHistoryRows, setCountHistoryRows] = React.useState([])
 
     function addCount() {
-        setCount(prevCount => prevCount + 1)
+        setCount(prevCount => {
+            setCountHistoryRows(prevArr => {
+                const row = (
+                    <tr>
+                        <td>{prevCount}</td>
+                        <td>+</td>
+                        <td>{prevCount + 1}</td>
+                    </tr>
+                )
+                return [...prevArr, row]
+            })
+            return prevCount + 1
+        })
     }
 
     function minusCount() {
-        setCount(prevCount => prevCount - 1)
+        setCount(prevCount => {
+            setCountHistoryRows(prevArr => {
+                const row = (
+                    <tr>
+                        <td>{prevCount}</td>
+                        <td>-</td>
+                        <td>{prevCount - 1}</td>
+                    </tr>
+                )
+                return [...prevArr, row]
+            })
+            return prevCount - 1
+        })
     }
 
     function resetCount() {
-        setCount(0)
+        setCount(prevCount => {
+            setCountHistoryRows(prevArr => {
+                const row = (
+                    <tr>
+                        <td>{prevCount}</td>
+                        <td>reset</td>
+                        <td>0</td>
+                    </tr>
+                )
+                return [...prevArr, row]
+            })
+            return 0
+        })
     }
 
     return (
-        <div className='countBlock'>
-            <h1>Press + or - to change the number</h1>
-            <h2 className='count'>{count}</h2>
-            <div className='buttons'>
-                <button onClick={minusCount}>-1</button>
-                <button onClick={resetCount}>reset</button>
-                <button onClick={addCount}>+1</button>
+        <>
+            <div className='countBlock'>
+                <h1>Press + or - to change the number</h1>
+                <h2 className='count'>{count}</h2>
+                <div className='buttons'>
+                    <button onClick={minusCount}>-1</button>
+                    <button onClick={resetCount}>reset</button>
+                    <button onClick={addCount}>+1</button>
+                </div>
+            <div className='count-history'>
+                <h4>count history</h4>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>prevCount</th>
+                            <th>operation</th>
+                            <th>newCount</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {countHistoryRows}
+                    </tbody>
+                </table>
             </div>
-        </div>
+            </div>
+        </>
     )
 }
